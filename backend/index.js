ee1170cdd91f55f5c0622e29a7f7f98542bdfd66
sparxrahulpawar <sparxoneman@gmail.com>
2024-10-29 12:56:14 +0530

    Create a TypeDefs and Resolver for graphQL (Backend)

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
-import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
- 
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
- 
-const { url } = await startStandaloneServer(server)
- 
-console.log(`🚀 Server ready at ${url}`)
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/`);
